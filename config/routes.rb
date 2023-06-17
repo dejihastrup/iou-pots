a53166ebace5a1020aad396cea605e089d3a31ec
Deji Hastrup <deji.hastrup@gmail.com>
2023-06-17 23:59:09 +0100

    Generated initial controllers and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :pots
+  resources :requests, only: [:index, :create, :update]
+  resources :payments, only: [:index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
