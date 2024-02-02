40e9d590c80bbaf5626ae465e3f248c5810683b8
Deji Hastrup <deji.hastrup@gmail.com>
2024-02-02 10:30:18 +0000

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
   def home
     @user = User.new
   end
+
+  def dashboard
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get 'dashboard', to: "pages#dashboard", as: 'dashboard'
   resources :pots
   resources :requests, only: [:index, :create, :update]
   resources :payments, only: [:index, :create]
